3100529beea54095b38aadb691c993f1b9f02f64
kaditya98829 <kaditya98829@gmail.com>
2023-06-06 21:43:13 +0530

    build-updated

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -9,30 +9,30 @@ export class UserService {
   constructor(private http: HttpClient) { }
   
   getUsers() {
-    return this.http.get('https://pahadihume/users');
+    return this.http.get('https://pahadihume.in.net/users');
   }
 
   registerUser(payload: {email: string, password: string, name: string}) {
-    return this.http.post('https://pahadihume/register', payload);
+    return this.http.post('https://pahadihume.in.net/register', payload);
   }
 
   login(paylaod: { email: string, password: string}) {
-    return this.http.post('https://pahadihume/login', paylaod);
+    return this.http.post('https://pahadihume.in.net/login', paylaod);
   }
  
   updateUser(id: string, paylaod: object) {
-    return this.http.put(`https://pahadihume/update/user/${id}`, paylaod);
+    return this.http.put(`https://pahadihume.in.net/update/user/${id}`, paylaod);
   }
 
   deleteUser(id: string) {
-    return this.http.delete(`https://pahadihume/delete/user/${id}`);
+    return this.http.delete(`https://pahadihume.in.net/delete/user/${id}`);
   }
 
  forgotPassword(payload: string) {
-  return this.http.post('https://pahadihume/forgot/password', {email: payload});
+  return this.http.post('https://pahadihume.in.net/forgot/password', {email: payload});
  }
 
  resetPassword(token: string, payload: object) {
-  return this.http.put(`https://pahadihume/reset/password/${token}`, payload)
+  return this.http.put(`https://pahadihume/reset.in.net/password/${token}`, payload)
  }
 }
